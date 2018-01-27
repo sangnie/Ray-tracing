7abb9a1a173e6dba724cc8476fe5007607d9b5eb
MukundMundhra <mukund8102@gmail.com>
2018-01-27 15:19:47 +0530

    Delete objects.c

--- objects.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "objects.h"
-
-point_3d int_sph(line l, sphere s);
-point_3d int_tri(line l, triangle tri);
-point_3d int_box(line l, box b);
-point_3d int_plane(line l, plane p);
-point_3d int_circle(line l, circle c);
